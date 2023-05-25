1a7a4d78b5ec0529c38877653d18d556ec31def6
3DUTEX <t3xdev@gmail.com>
2023-05-25 11:00:19 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function menuShow() {
+    let menuMobile = document.querySelector(".mobile-menu");
+    const menuIcon = document.querySelector(".icon");
+
+    if (menuMobile.classList.contains("open")) {
+        menuMobile.classList.remove("open")
+
+        menuIcon.src = "images/menu_white_36dp.svg"
+    } else {
+        menuIcon.src = "images/close_white_36dp.svg"
+        
+        menuMobile.classList.add("open")     
+    }
+}
