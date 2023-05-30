a0befc986c90fee72da3d10db647c7dc1c8c732c
3DUTEX <132105117+3DUTEX@users.noreply.github.com>
2023-05-30 17:15:03 -0300

    links footerbar and div locar added

--- main.js
+++ main.js
@@ -13,3 +13,24 @@ function menuShow() {
     }
 }
 
+let sla = document.querySelector("#btn")
+let locar = document.querySelector(".locar")
+let X = document.querySelector("#X")
+
+let cont = 0;
+sla.addEventListener("click", function(){
+    
+    if(cont == 1){
+        alert("Preencha os campos")   
+    };
+
+    locar.style.display = "block";
+    
+    cont = 1;
+
+})
+
+X.addEventListener("click", function(){
+    locar.style.display = "none";
+    cont = 0;
+})
