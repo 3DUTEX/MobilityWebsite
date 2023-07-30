5b701e8c005ef51f49b033c77442ed3c17c9b20c
Eduardo <132105117+3DUTEX@users.noreply.github.com>
2023-07-30 13:24:38 -0300

    playlist modal created

--- main.js
+++ main.js
@@ -73,3 +73,20 @@ window.addEventListener("scroll", () => {
 
   indicator.style.width = `${percentage}%`;
 });
+
+const playlistImg = document.querySelector("#playlistImg");
+const playlistModal = document.querySelector("#playlistModal");
+
+playlistImg.addEventListener("click", () => {
+  if (playlistModal.className === "hidden") {
+    playlistModal.className = "visible";
+  } else {
+    playlistModal.className = "hidden";
+  }
+});
+
+const closeModal = document.querySelector("#closeModal");
+
+closeModal.addEventListener("click", () => {
+  playlistModal.className = "hidden";
+});
